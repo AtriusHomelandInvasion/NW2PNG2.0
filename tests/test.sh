4387678a517250ebaed35595a62033ccf426252f
Chris Vimes <chris@graalcenter.org>
2011-11-26 08:44:07 -0500

    Adjusting test script for new location of CLI JAR (now it's in the main directory).

--- tests/test.sh
+++ tests/test.sh
@@ -2,7 +2,7 @@
 
 TEST_START_TIME=$(date +%s)
 
-java -jar NW2PNG-cli.jar \
+java -jar ../NW2PNG-cli.jar \
 --graaldir "~/Application Support/Graal" \
 --tileset classic_collaboration/collaboration_tileset.png \
 --input classic_collaboration/classic_collaboration.gmap \
