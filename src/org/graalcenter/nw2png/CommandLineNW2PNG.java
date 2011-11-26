81b1c2d0ef1aea1e3d4a8347bcbc2d7f584072e5
Chris Vimes <chris@graalcenter.org>
2011-11-26 07:31:47 -0500

    Finishing CLI, tested and working well.

--- src/org/graalcenter/nw2png/CommandLineNW2PNG.java
+++ src/org/graalcenter/nw2png/CommandLineNW2PNG.java
@@ -1,27 +1,102 @@
 package org.graalcenter.nw2png;
 
 import born2kill.nw2png.Listener;
+import born2kill.nw2png.NW2PNGHelper;
+import java.io.File;
 import java.io.IOException;
 import joptsimple.OptionParser;
 import joptsimple.OptionSet;
 
 
 public class CommandLineNW2PNG implements Listener {
-    public static void main(String[] args) throws IOException {
-        // required arguments:
-        //      tileset
-        //      input file
-        //      output file
+    private NW2PNGHelper helper;
+    
+    public CommandLineNW2PNG(String graalDir, File tileset, File input, File output, boolean renderNPCs, boolean filter, boolean split, double scale) {
+        helper = new NW2PNGHelper(this);
         
+        // set options
+        try {
+            helper.setGraalDir(graalDir);
+            helper.setTileset(tileset);
+            helper.setSourceFile(input);
+            helper.setOutputFile(output);
+            helper.setRenderNPCs(renderNPCs);
+            helper.setFilter(filter);
+            helper.setSplit(split);
+            helper.setScale(scale);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            System.exit(0);
+        }
+        
+        // run
+        helper.generate();
+    }
+    
+    public static void main(String[] args) throws IOException {
         OptionParser parser = new OptionParser() {
             {
-                accepts("t", "Path to the tileset file").withRequiredArg().ofType(String.class).describedAs("path to tileset");
-                accepts("i", "Path to the input file (GMAP or NW)").withRequiredArg().ofType(String.class).describedAs("path to level");
-                accepts("o", "Path to the generated PNG").withRequiredArg().ofType(String.class).describedAs("path to PNG");
+                // required arguments
+                accepts("graaldir", "Path to the Graal directory (req)").withRequiredArg().ofType(String.class).describedAs("path to graal dir");
+                accepts("tileset", "Path to the tileset file (req)").withRequiredArg().ofType(String.class).describedAs("path to tileset");
+                accepts("input", "Path to the input file (GMAP or NW) (req)").withRequiredArg().ofType(String.class).describedAs("path to level");
+                accepts("output", "Path to the generated PNG (req)").withRequiredArg().ofType(String.class).describedAs("path to PNG");
+                
+                // options
+                accepts("skip-npcs", "Don't render NPCs");
+                accepts("skip-filter", "Don't filter output");
+                accepts("split", "Split GMAP into levels");
+                
+                // optional arguments
+                accepts("scale", "Scale as a double (e.g. 1, 0.5, 0.25)").withRequiredArg().ofType(Double.class).defaultsTo(1.0).describedAs("scale of output");
             }
         };
         
-        OptionSet options = parser.parse(args);
+        if (args.length <= 0 || args[0].equals("help")) {
+            parser.printHelpOn(System.out);
+        } else {
+            OptionSet options = parser.parse(args);
+            
+            String[] requiredArguments = {"graaldir", "tileset", "input", "output"};
+            
+            for (String requiredArgument : requiredArguments) {
+                if (! options.hasArgument(requiredArgument)) {
+                    System.err.println("Missing required argument: " + requiredArgument);
+                    System.exit(0);
+                }
+            }
+            
+            String graalDir = (String) options.valueOf("graaldir");
+            File tileset = new File((String) options.valueOf("tileset"));
+            File input = new File((String) options.valueOf("input"));
+            File output = new File((String) options.valueOf("output"));
+            
+            boolean renderNPCs = ! options.has("skip-npcs");
+            boolean filter = ! options.has("skip-filter");
+            boolean split = options.has("split");
+            
+            double scale = Double.valueOf(options.valueOf("scale").toString());
+            
+            System.out.println("Specified Options:");
+            
+            Object[][] printedOptions = {
+                {"Graal Dir", graalDir},
+                {"Tileset", tileset},
+                {"Input", input},
+                {"Output", output},
+                {"Render NPCs", renderNPCs},
+                {"Filter", filter},
+                {"Split", split},
+                {"Scale", scale}
+            };
+            
+            for (Object[] printedOption : printedOptions) {
+                System.out.println("\t" + printedOption[0] + ": " + printedOption[1]);
+            }
+            
+            new CommandLineNW2PNG(graalDir, tileset, input, output, renderNPCs, filter, split, scale);
+        }
+        
     }
 
     public void sendMessage(String message) {
